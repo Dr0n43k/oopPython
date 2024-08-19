79f443397076b0b874fb267d3afedb0ecd53395c
dr0n43k <otto8bit@gmail.com>
2024-08-19 23:12:37 +0300

    imports fix

--- lesson_3/views/console_ui.py
+++ lesson_3/views/console_ui.py
@@ -1,16 +1,17 @@
 import datetime
-from oopPython.lesson_3.views.view import View
-from oopPython.lesson_3.presenter import Presenter
-import text
+from views.view import View
+from presenter.presenter import Presenter
+import views.text as text
 
 class Ui(View):
   def __init__(self):
     self.__presenter = Presenter(self)
   def print_answer(answer : str):
     return answer
-  def imput_answer():
+  def imput_answer(self):
     return int(str(input(text.imput_number)))
   def start(self):
+    print(text.menu)
     while True:
       choise = self.imput_answer()
       match choise:
@@ -19,7 +20,7 @@ class Ui(View):
             case 2:
                 name = str(input("Введите имя: "))
                 gender = str(input("Введите пол: "))
-                date = datetime.datetime.strptime(str(input(("Введите дату рождения"))), "%d.%m.%Y")
+                date = input(("Введите дату рождения"))
                 self.__presenter.append_human(name, gender, date)
                 print("Успешно добавлено")
             case 3:
@@ -30,3 +31,4 @@ class Ui(View):
                 self.__presenter.sorting_name()
             case 6:
                 print(text.menu_close)
+                return
